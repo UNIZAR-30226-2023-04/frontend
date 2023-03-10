5b46c8f28475adb6b826c3822320c5a5957ec3d9
Luis Catalán Salas <774274@unizar.es>
2023-03-10 19:29:42 +0100

    Login hecho

--- decibely/src/app/app-routing.module.ts
+++ decibely/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'register', component: RegisterComponent },
-  { path: '', redirectTo: '/register', pathMatch: 'full' }
+  { path: '', redirectTo: '/register', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
